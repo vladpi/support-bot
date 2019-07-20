52cca9ef525f27e0c7f95e30142d6ab50b5b15a7
Vlad Pi <vladp@ro.ru>
2019-07-21 01:11:41 +0300

    Added logging to VK and TG bots

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,40 +1,66 @@
+import logging
 import os
 
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from dialogflow_utils import get_answer
+from logs_bot import BotLogsHandler
+
+logger = logging.getLogger('Support Telegram Bot')
 
 
 def handle_start(bot, update):
     """ Handle start command """
 
-    update.message.reply_text('Здравствуйте')
+    logger.debug('Received /start command')
+    answer = 'Здравствуйте'
+    update.message.reply_text(answer)
+    logger.debug('Send reply message: {answer}'.format(answer=answer))
 
 
 def handle_message(bot, update):
     """ Handle all incoming text messages from client with the DialogFlow agent """
 
+    logger.debug('Received text message: {message}'.format(message=update.message.text))
     answer, _ = get_answer(update.message.text, update.message.chat_id)
     update.message.reply_text(answer)
+    logger.debug('Send reply message: {answer}'.format(answer=answer))
 
 
 def main():
-    load_dotenv()
+    try:
+        bot_token = os.environ.get('TG_TOKEN')
 
-    bot_token = os.environ.get('TG_TOKEN')
+        updater = Updater(bot_token)
+        dispatcher = updater.dispatcher
 
-    updater = Updater(bot_token)
-    dispatcher = updater.dispatcher
+        start_handler = CommandHandler('start', handle_start)
+        echo_handler = MessageHandler(Filters.text, handle_message)
 
-    start_handler = CommandHandler('start', handle_start)
-    echo_handler = MessageHandler(Filters.text, handle_message)
+        dispatcher.add_handler(start_handler)
+        dispatcher.add_handler(echo_handler)
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
+        logger.info('Start polling')
+        updater.start_polling()
+        logger.info('End polling')
 
-    updater.start_polling()
+    except Exception as ex:
+        logger.error(ex, exc_info=True)
 
 
 if __name__ == '__main__':
+    load_dotenv()
+
+    logger.setLevel(logging.INFO)
+
+    logs_bot_token = os.environ.get('LOGS_BOT_TOKEN')
+    author_chat_id = os.environ.get('AUTHOR_CHAT_ID')
+
+    if logs_bot_token and author_chat_id:
+        logger.addHandler(BotLogsHandler(logs_bot_token, author_chat_id))
+
+    else:
+        logger.addHandler(logging.StreamHandler())
+
     main()
--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import random
 
@@ -6,10 +7,15 @@ from vk_api import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from dialogflow_utils import get_answer
+from logs_bot import BotLogsHandler
+
+logger = logging.getLogger('Support VK Bot')
 
 
 def handle_message(event, session_api):
     """ Handle all incoming messages from client with the DialogFlow agent """
+
+    logger.debug('Received text message: {message}'.format(message=event.text))
 
     answer, intent = get_answer(event.text, event.user_id)
 
@@ -19,23 +25,43 @@ def handle_message(event, session_api):
             message=answer,
             random_id=random.randint(1, 1000)
         )
+        logger.debug('Send reply message: {answer}'.format(answer=answer))
 
 
 def main():
     """ Long polling Bot for VK group """
 
-    load_dotenv()
+    try:
+        vk_token = os.environ.get('VK_TOKEN')
 
-    vk_token = os.environ.get('VK_TOKEN')
+        vk_session = vk_api.VkApi(token=vk_token)
+        session_api = vk_session.get_api()
+        polling = VkLongPoll(vk_session)
 
-    vk_session = vk_api.VkApi(token=vk_token)
-    session_api = vk_session.get_api()
-    polling = VkLongPoll(vk_session)
+        logger.info('Start polling')
 
-    for event in polling.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            handle_message(event, session_api)
+        for event in polling.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                handle_message(event, session_api)
+
+        logger.info('End polling')
+
+    except Exception as ex:
+        logger.error(ex, exc_info=True)
 
 
 if __name__ == '__main__':
+    load_dotenv()
+
+    logger.setLevel(logging.INFO)
+
+    logs_bot_token = os.environ.get('LOGS_BOT_TOKEN')
+    author_chat_id = os.environ.get('AUTHOR_CHAT_ID')
+
+    if logs_bot_token and author_chat_id:
+        logger.addHandler(BotLogsHandler(logs_bot_token, author_chat_id))
+
+    else:
+        logger.addHandler(logging.StreamHandler())
+
     main()
